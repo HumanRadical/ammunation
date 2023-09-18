7eaf71db73b677e56efe1c95bc1ef8f439095717
Easton Meth <easton.meth@gmail.com>
2023-09-18 13:12:03 -0400

    Update application logo

--- resources/js/components/Navbar.jsx
+++ resources/js/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import { Link } from '@inertiajs/react';
+import ApplicationLogo from './ApplicationLogo';
 
 export default function Navbar(props) {
     return (
         <>
             <div className="p-6 bg-red-600 w-full grid grid-cols-8">
                 <a className='col-start-4 col-span-2' href="/">
-                    <img src='http://localhost:8000/images/ammunation_logo.png' alt="" />
+                    <ApplicationLogo />
                 </a>
                 <div className='flex justify-end content-center col-end-9'>
                     {props.auth.user ? (
