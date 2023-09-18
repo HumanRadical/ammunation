d2ca222f6ec62c77eca8622150ff155dfeebaa39
Easton Meth <easton.meth@gmail.com>
2023-09-18 14:44:02 -0400

    Update instances of application logo to be application icon

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import ApplicationLogo from '@/Components/ApplicationLogo';
+import ApplicationIcon from '@/Components/ApplicationIcon';
 import Dropdown from '@/Components/Dropdown';
 import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
@@ -16,7 +16,7 @@ export default function Authenticated({ user, header, children }) {
                         <div className="flex">
                             <div className="shrink-0 flex items-center">
                                 <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                                    <ApplicationIcon className="block w-10 w-auto fill-current text-gray-800" />
                                 </Link>
                             </div>
 
---
+++ resources/js/components/ApplicationIcon.jsx
@@ -0,0 +1,5 @@
+export default function ApplicationIcon(props) {
+    return (
+        <img {...props} src='http://localhost:8000/images/ammunation_icon.jpg' alt="" />
+    );
+}
