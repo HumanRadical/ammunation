1339916ec06010f559c29fd84b29759e416aa0a7
Easton Meth <easton.meth@gmail.com>
2023-09-18 13:10:57 -0400

    Pass products through to home page

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,13 +1,17 @@
 import Navbar from '@/components/Navbar';
 import { Head } from '@inertiajs/react';
 
-export default function Home({ auth }) {
+export default function Home({ auth, products }) {
+    const productDisplays = products.map(product => {
+        return <h1>{ product.name }</h1>
+    })
+
     return (
         <>
             <Head title="Home" />
             <Navbar auth={auth} />
             <main className='text-center'>
-                <h1>Welcome to Ammunation!</h1>
+                { productDisplays }
             </main>
         </>
     );
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -8,6 +9,7 @@ Route::get('/', function () {
     return Inertia::render('Home', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
+        'products' => Product::all()
     ]);
 });
 
