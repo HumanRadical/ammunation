cc5fcf574b4054362345471d8cddd44f48450fbf
Easton Meth <easton.meth@gmail.com>
2023-09-18 14:57:57 -0400

    Create filter bar on home page

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -9,8 +9,22 @@ export default function Home({ auth, products }) {
 
     return (
         <>
-            <Head title="Home" />
+            <Head title='Home' />
             <Navbar auth={auth} />
+            <div className='p-3 border-b border-gray-300 flex justify-center'>
+                <form action='/'>
+                    <label htmlFor="category">Category: </label>
+                    <select className='mr-3' name='category' id='category'>
+                        <option value='handgun'>Handgun</option>
+                    </select>
+                    <label htmlFor="price">Price Range: </label>
+                    <select className='mr-3' name='price' id='price'>
+                        <option value='price'>$100 - $5000</option>
+                    </select>
+                    <label htmlFor="price">Search: </label>
+                    <input type="text" placeholder='Glock-19...' id='search' name='search' />
+                </form>
+            </div>
             <main className='flex flex-wrap p-10 m-auto'>
                 { productCards }
             </main>
