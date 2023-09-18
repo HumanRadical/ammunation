3e09a8be077a1bc0b137edcd9284005fcf931095
Easton Meth <easton.meth@gmail.com>
2023-09-18 15:01:46 -0400

    Update product factory and seeder

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -17,7 +17,7 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => ucfirst(fake()->word()) . ' ' . fake()->randomNumber(2, false),
+            'name' => ucfirst(fake()->word()) . '-' . fake()->randomNumber(2, false),
             'slug' => fake()->slug(),
             'category' => ucfirst(fake()->word()),
             'price' => fake()->randomFloat(2, 100, 5000),
