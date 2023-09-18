3103d7e951b96628613906a9bd59828444c4ae93
Easton Meth <easton.meth@gmail.com>
2023-09-18 13:36:12 -0400

    Create product card component

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,16 +1,17 @@
 import Navbar from '@/components/Navbar';
+import ProductCard from '@/components/ProductCard';
 import { Head } from '@inertiajs/react';
 
 export default function Home({ auth, products }) {
     const productDisplays = products.map(product => {
-        return <h1>{ product.name }</h1>
+        return <ProductCard product={product} />
     })
 
     return (
         <>
             <Head title="Home" />
             <Navbar auth={auth} />
-            <main className='text-center'>
+            <main className='flex flex-wrap p-10 m-auto'>
                 { productDisplays }
             </main>
         </>
---
+++ resources/js/components/ProductCard.jsx
@@ -0,0 +1,10 @@
+export default function ProductCard({ product }) {
+    return (
+        <a href={'/products/' + product.slug } className='border border-black rounded-xl m-3 p-5'>
+            <img className='border border-black p-3 max-w-sm bg-gray-300' src='http://localhost:8000/images/gun_icon.png' alt='Gun'/>
+            <h3 className='font-bold text-lg mt-3'>{ product.name }</h3>
+            <h4 className='text-gray-500'>{ product.category }</h4>
+            <h5 className='text-green-500'>${ product.price }</h5>
+        </a>
+    );
+}
