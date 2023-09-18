235288f6f04af923ec2b7f59d3d96d5fa8fc9228
Easton Meth <easton.meth@gmail.com>
2023-09-18 14:06:08 -0400

    Styling

--- resources/js/Pages/ProductPage.jsx
+++ resources/js/Pages/ProductPage.jsx
@@ -7,9 +7,9 @@ export default function ProductPage({ auth, product }) {
         <>
             <Head title={product.name} />
             <Navbar auth={auth} />
-            <div className='mt-10 max-w-5xl mx-auto p-10'>
+            <div className='max-w-4xl mt-10 mx-auto'>
                 <img className='border border-black p-3 bg-gray-300 mx-auto' src='http://localhost:8000/images/gun_icon.png' alt='Gun' width='500' />
-                <h2 className='font-bold text-4xl'>{ product.name }</h2>
+                <h2 className='font-bold text-4xl mt-6'>{ product.name }</h2>
                 <h3 className='text-gray-500 text-2xl'>{ product.category }</h3>
                 <h4 className='text-green-500 text-2xl'>${ product.price }</h4>
                 <p className='text-lg mt-4'>{ product.description }</p>
