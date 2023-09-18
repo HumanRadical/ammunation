e301a971e7b95e762dfca7099eb2a7f7afdea4a0
Easton Meth <easton.meth@gmail.com>
2023-09-18 14:31:13 -0400

    Update navbar to use nav instead of div

--- resources/js/components/Navbar.jsx
+++ resources/js/components/Navbar.jsx
@@ -3,38 +3,36 @@ import ApplicationLogo from './ApplicationLogo';
 
 export default function Navbar(props) {
     return (
-        <>
-            <div className="p-6 bg-red-600 w-full grid grid-cols-8">
-                <a className='col-start-4 col-span-2' href="/">
-                    <ApplicationLogo />
-                </a>
-                <div className='flex justify-end content-center col-end-9'>
-                    {props.auth.user ? (
+        <nav className="p-6 bg-red-600 w-full grid grid-cols-8">
+            <a className='col-start-4 col-span-2' href="/">
+                <ApplicationLogo />
+            </a>
+            <div className='flex justify-end content-center col-end-9'>
+                {props.auth.user ? (
+                    <Link
+                        href={route('dashboard')}
+                        className="font-semibold text-xl text-white my-auto"
+                    >
+                        Dashboard
+                    </Link>
+                ) : (
+                    <>
                         <Link
-                            href={route('dashboard')}
+                            href={route('login')}
                             className="font-semibold text-xl text-white my-auto"
                         >
-                            Dashboard
+                            Log in
                         </Link>
-                    ) : (
-                        <>
-                            <Link
-                                href={route('login')}
-                                className="font-semibold text-xl text-white my-auto"
-                            >
-                                Log in
-                            </Link>
 
-                            <Link
-                                href={route('register')}
-                                className="ml-4 font-semibold text-xl text-white my-auto"
-                            >
-                                Register
-                            </Link>
-                        </>
-                    )}
-                </div>
+                        <Link
+                            href={route('register')}
+                            className="ml-4 font-semibold text-xl text-white my-auto"
+                        >
+                            Register
+                        </Link>
+                    </>
+                )}
             </div>
-        </>
+        </nav>
     )
 }
