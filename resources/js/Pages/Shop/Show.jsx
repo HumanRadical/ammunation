34d921906d6cb70a455b277a52489b4a8fd114c7
Easton Meth <easton.meth@gmail.com>
2023-09-19 16:57:56 -0400

    Update styles

--- resources/js/Pages/Shop/Show.jsx
+++ resources/js/Pages/Shop/Show.jsx
@@ -4,12 +4,12 @@ import { Link } from '@inertiajs/react';
 export default function Show({ auth, product }) {
     return (
         <Layout auth={auth} head={product.name}>
-            <div className='grid grid-cols-3'>
-                <Link href={route('shop.index')} className='m-8 max-h-10 flex content-center'>
+            <div className='grid grid-cols-3 m-10'>
+                <Link href={route('shop.index')} className='max-h-10 flex content-center'>
                     <img className='w-10' src="http://localhost:8000/images/chevron-left-solid.svg" alt=""/>
                     <span className='text-2xl font-bold my-auto'>Back</span>
                 </Link>
-                <div className='max-w-4xl mt-10 mx-auto'>
+                <div className='max-w-4xl mx-auto'>
                     <img className='border border-black p-3 bg-gray-300 mx-auto' src='http://localhost:8000/images/gun_icon.png' alt='Gun' width='500' />
                     <h2 className='font-bold text-4xl mt-6'>{product.name}</h2>
                     <h3 className='text-gray-500 text-2xl'>{product.category.name}</h3>
