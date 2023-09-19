8c173e4c09644fb0a9a8165893d388519cef60c7
Easton Meth <easton.meth@gmail.com>
2023-09-19 17:45:48 -0400

    Update home page

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -3,7 +3,10 @@ import Layout from '@/Layouts/Layout';
 export default function Home({ auth }) {
     return (
         <Layout auth={auth} head='Home'>
-            <h2>Welcome to ammunation!</h2>
+            <div className='grid grid-cols-9 grid-rows-6'>
+                <img className='w-full col-span-full row-span-full' src='http://localhost:8000/images/ammunation_background2.png' />
+                <button className='text-white text-2xl font-bold h-12 bg-green-600 rounded-lg border-2 border-black col-start-5 row-start-6'>Shop Now</button>
+            </div>
         </Layout>
     );
 }
