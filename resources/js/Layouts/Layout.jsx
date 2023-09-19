4240d2fdf5bca599369c63e6cac410e70651fca0
Easton Meth <easton.meth@gmail.com>
2023-09-19 17:38:00 -0400

    Update layout file to accept extra props

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -2,13 +2,15 @@ import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 import { Head } from '@inertiajs/react';
 
-export default function Home({ auth, head, children }) {
+export default function Home({ auth, head, children, ...props }) {
 
     return (
         <>
             <Head title={head} />
             <Navbar auth={auth} />
-            {children}
+            <div {...props}>
+                {children}
+            </div>
             <Footer />
         </>
     );
