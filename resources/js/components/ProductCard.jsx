0878c1a06a2d09809c3b6cdf300f772610f204f4
Easton Meth <easton.meth@gmail.com>
2023-09-19 14:41:54 -0400

    Update product card to use inertia link

--- resources/js/components/ProductCard.jsx
+++ resources/js/components/ProductCard.jsx
@@ -1,10 +1,12 @@
+import { Link } from '@inertiajs/react';
+
 export default function ProductCard({ product }) {
     return (
-        <a href={'/shop/' + product.slug} className='border border-black rounded-xl m-3 p-5'>
+        <Link href={route('shop.show', { product: product })} className='border border-black rounded-xl m-3 p-5'>
             <img className='border border-black p-3 max-w-sm bg-gray-300' src='http://localhost:8000/images/gun_icon.png' alt='Gun'/>
             <h3 className='font-bold text-lg mt-3'>{product.name}</h3>
             <h4 className='text-gray-500'>{product.category.name}</h4>
             <h5 className='text-green-500'>${product.price}</h5>
-        </a>
+        </Link>
     );
 }
