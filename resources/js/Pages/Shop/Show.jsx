b06cb1b4056ae0a35b674b2b052febe56e5777e2
Easton Meth <easton.meth@gmail.com>
2023-09-19 15:04:56 -0400

    Add back button to show page

--- resources/js/Pages/Shop/Show.jsx
+++ resources/js/Pages/Shop/Show.jsx
@@ -1,14 +1,21 @@
 import Layout from '@/Layouts/Layout';
+import { Link } from '@inertiajs/react';
 
 export default function Show({ auth, product }) {
     return (
         <Layout auth={auth} head={product.name}>
-            <div className='max-w-4xl mt-10 mx-auto'>
-                <img className='border border-black p-3 bg-gray-300 mx-auto' src='http://localhost:8000/images/gun_icon.png' alt='Gun' width='500' />
-                <h2 className='font-bold text-4xl mt-6'>{product.name}</h2>
-                <h3 className='text-gray-500 text-2xl'>{product.category.name}</h3>
-                <h4 className='text-green-500 text-2xl'>${product.price}</h4>
-                <p className='text-lg mt-4'>{product.description}</p>
+            <div className='grid grid-cols-3'>
+                <Link href={route('shop.index')} className='m-8 max-h-10 flex content-center'>
+                    <img className='w-10' src="http://localhost:8000/images/chevron-left-solid.svg" alt=""/>
+                    <span className='text-2xl font-bold my-auto'>Back</span>
+                </Link>
+                <div className='max-w-4xl mt-10 mx-auto'>
+                    <img className='border border-black p-3 bg-gray-300 mx-auto' src='http://localhost:8000/images/gun_icon.png' alt='Gun' width='500' />
+                    <h2 className='font-bold text-4xl mt-6'>{product.name}</h2>
+                    <h3 className='text-gray-500 text-2xl'>{product.category.name}</h3>
+                    <h4 className='text-green-500 text-2xl'>${product.price}</h4>
+                    <p className='text-lg mt-4'>{product.description}</p>
+                </div>
             </div>
         </Layout>
     );
