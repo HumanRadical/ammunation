c6e31fc4512ed30f7b40158b4f27de80b397175b
Easton Meth <easton.meth@gmail.com>
2023-09-19 18:16:08 -0400

    Rename left chevron svg

--- resources/js/Pages/Shop/Show.jsx
+++ resources/js/Pages/Shop/Show.jsx
@@ -6,7 +6,7 @@ export default function Show({ auth, product }) {
         <Layout auth={auth} head={product.name}>
             <div className='grid grid-cols-3 m-10'>
                 <Link href={route('shop.index')} className='max-h-10 flex content-center'>
-                    <img className='w-10' src="http://localhost:8000/images/chevron-left-solid.svg" alt=""/>
+                    <img className='w-10' src="http://localhost:8000/images/chevron-left.svg" alt=""/>
                     <span className='text-2xl font-bold my-auto'>Back</span>
                 </Link>
                 <div className='max-w-4xl mx-auto'>
