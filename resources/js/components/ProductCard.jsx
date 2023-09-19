9e296b8a61343a2cada125b9a0a4967b31455fb1
Easton Meth <easton.meth@gmail.com>
2023-09-18 20:01:10 -0400

    Update product card link

--- resources/js/components/ProductCard.jsx
+++ resources/js/components/ProductCard.jsx
@@ -1,6 +1,6 @@
 export default function ProductCard({ product }) {
     return (
-        <a href={'/products/' + product.slug} className='border border-black rounded-xl m-3 p-5'>
+        <a href={'/shop/' + product.slug} className='border border-black rounded-xl m-3 p-5'>
             <img className='border border-black p-3 max-w-sm bg-gray-300' src='http://localhost:8000/images/gun_icon.png' alt='Gun'/>
             <h3 className='font-bold text-lg mt-3'>{product.name}</h3>
             <h4 className='text-gray-500'>{product.category.name}</h4>
