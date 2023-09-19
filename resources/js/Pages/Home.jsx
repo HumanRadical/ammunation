e93ca6374dbd35e906eb113b2de393d167836d1f
Easton Meth <easton.meth@gmail.com>
2023-09-19 18:36:09 -0400

    Styling

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -13,11 +13,11 @@ export default function Home({ auth, categories }) {
                 <img className='w-full col-span-full row-span-full' src='http://localhost:8000/images/ammunation_background2.png' />
                 <button className='text-white text-2xl font-bold h-12 bg-green-500 hover:bg-green-600 transition rounded-lg col-start-5 row-start-6'>
                     <Link href={route('shop.index')} className='w-full block'>
-                        Shop Now
+                        Shop Now!
                     </Link>
                 </button>
             </div>
-            <h2 className='text-3xl font-bold text-center mt-6 mb-2'>Shop by Category:</h2>
+            <h2 className='text-3xl text-center mt-6 mb-2'>Shop by Category:</h2>
             <div className='w-1/2 mx-auto grid grid-cols-2 grid-rows-2 mb-8'>
                 {categoryCards}
             </div>
