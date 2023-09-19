139d9d2c5addec037260c37c90bc43ef5d3e1472
Easton Meth <easton.meth@gmail.com>
2023-09-19 17:46:04 -0400

    Update home page

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,11 +1,16 @@
 import Layout from '@/Layouts/Layout';
+import { Link } from '@inertiajs/react';
 
 export default function Home({ auth }) {
     return (
         <Layout auth={auth} head='Home'>
             <div className='grid grid-cols-9 grid-rows-6'>
                 <img className='w-full col-span-full row-span-full' src='http://localhost:8000/images/ammunation_background2.png' />
-                <button className='text-white text-2xl font-bold h-12 bg-green-600 rounded-lg border-2 border-black col-start-5 row-start-6'>Shop Now</button>
+                <button className='text-white text-2xl font-bold h-12 bg-green-600 rounded-lg col-start-5 row-start-6'>
+                    <Link href={route('shop.index')} className='w-full block'>
+                        Shop Now
+                    </Link>
+                </button>
             </div>
         </Layout>
     );
