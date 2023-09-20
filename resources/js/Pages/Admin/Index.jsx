eab567ac82e1cde86a7f117c385f6eafb79887e9
Easton Meth <easton.meth@gmail.com>
2023-09-20 13:09:37 -0400

    Update header and head in admin index

--- resources/js/Pages/Admin/Index.jsx
+++ resources/js/Pages/Admin/Index.jsx
@@ -56,9 +56,9 @@ export default function Dashboard({ auth, products }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Products</h2>}
         >
-            <Head title="Dashboard" />
+            <Head title="Products" />
 
             <div className="py-12">
                 <div className="max-w-5xl mx-auto sm:px-6 lg:px-8">
