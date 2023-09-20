8fc72df4d434dae7ccbbcb61b386eb9f9a94ddc9
Easton Meth <easton.meth@gmail.com>
2023-09-20 13:38:28 -0400

    Layout styling

--- resources/js/Layouts/MainLayout.jsx
+++ resources/js/Layouts/MainLayout.jsx
@@ -5,13 +5,15 @@ import { Head } from '@inertiajs/react';
 export default function MainLayout({ auth, head, children, ...props }) {
 
     return (
-        <>
-            <Head title={head} />
-            <Navbar auth={auth} />
-            <div {...props}>
-                {children}
+        <div className='min-h-screen grid grid-rows-none'>
+            <div>
+                <Head title={head} />
+                <Navbar auth={auth} />
+                <div {...props}>
+                    {children}
+                </div>
             </div>
             <Footer />
-        </>
+        </div>
     );
 }
--- resources/js/components/Footer.jsx
+++ resources/js/components/Footer.jsx
@@ -2,7 +2,7 @@ import ApplicationIcon from './ApplicationIcon';
 
 export default function Footer() {
     return (
-        <footer className="p-6 bg-red-700 w-full text-white flex justify-center space-x-3">
+        <footer className="p-6 bg-red-700 w-full text-white flex justify-center space-x-3 self-end">
             <ApplicationIcon className='w-10 inline' />
             <p className='my-auto'>2023 Ammunation Inc.&trade;</p>
         </footer>
