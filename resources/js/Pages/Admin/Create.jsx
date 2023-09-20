7c0309fbab6f506b5bbe48e6825ce64907ed3b60
Easton Meth <easton.meth@gmail.com>
2023-09-20 14:21:51 -0400

    Build and style create form

--- resources/js/Pages/Admin/Create.jsx
+++ resources/js/Pages/Admin/Create.jsx
@@ -1,7 +1,11 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head, Link } from '@inertiajs/react';
+import { Head } from '@inertiajs/react';
 
 export default function Create({ auth }) {
+    const handleSubmit = () => {
+        //
+    }
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -9,7 +13,23 @@ export default function Create({ auth }) {
         >
             <Head title="New Product" />
 
-            <p>Hi!</p>
+            <div className="max-w-5xl mx-auto sm:px-6 lg:px-8 py-12 flex flex-col">
+                <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <form onSubmit={handleSubmit} className='flex flex-col mx-auto max-w-3xl p-12'>
+                        <label className='text-gray-700 text-xl tracking-wide' htmlFor='name'>NAME</label>
+                        <input className='w-full border-gray-300 rounded-md mt-1' type="text" name='name' id='name' />
+                        <label className='text-gray-700 text-xl tracking-wide mt-8' htmlFor='category'>CATEGORY</label>
+                        <input className='w-full border-gray-300 rounded-md mt-1' type="text" name='category' id='category' />
+                        <label className='text-gray-700 text-xl tracking-wide mt-8' htmlFor='price'>PRICE</label>
+                        <input className='w-full border-gray-300 rounded-md mt-1' type="text" name='price' id='price' />
+                        <label className='text-gray-700 text-xl tracking-wide mt-8' htmlFor='image'>IMAGE</label>
+                        <input className='mt-1' type="file" name='image' id='image' />
+                        <label className='text-gray-700 text-xl tracking-wide mt-8' htmlFor='description'>DESCRIPTION</label>
+                        <textarea className='w-full border-gray-300 rounded-md mt-1' name='description' id='description' rows='50'></textarea>
+                        <button className=' bg-blue-500 max-w-min px-8 py-3 mt-8 mx-auto rounded-xl text-white text-xl tracking-wide'>SUBMIT</button>
+                    </form>
+                </div>
+            </div>
 
         </AuthenticatedLayout>
     );
