b73969b79be668962629ee5f4561723de01692b1
Easton Meth <easton.meth@gmail.com>
2023-09-19 22:52:31 -0400

    Increase border on show page

--- resources/js/Pages/Shop/Show.jsx
+++ resources/js/Pages/Shop/Show.jsx
@@ -10,7 +10,7 @@ export default function Show({ auth, product }) {
                     <span className='text-2xl font-bold my-auto'>Back</span>
                 </Link>
                 <div className='max-w-4xl mx-auto'>
-                    <img className='border border-black p-3 bg-gray-300 mx-auto' src='http://localhost:8000/images/gun_icon.png' alt='Gun' width='500' />
+                    <img className='border-4 border-black p-3 bg-gray-300 mx-auto' src='http://localhost:8000/images/gun_icon.png' alt='Gun' width='500' />
                     <h2 className='font-bold text-4xl mt-6'>{product.name}</h2>
                     <h3 className='text-gray-500 text-2xl'>{product.category.name}</h3>
                     <h4 className='text-green-500 text-2xl'>${product.price}</h4>
