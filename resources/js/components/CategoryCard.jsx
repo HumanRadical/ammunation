5e69748a25a893f27ca4d62c97565b849847f914
Easton Meth <easton.meth@gmail.com>
2023-09-21 16:19:38 -0400

    Change category links to use link instead of router

--- resources/js/components/CategoryCard.jsx
+++ resources/js/components/CategoryCard.jsx
@@ -1,13 +1,13 @@
-import { router } from '@inertiajs/react';
+import { Link } from '@inertiajs/react';
 
 export default function CategoryCard({ category }) {
     return (
-        <button onClick={() => router.get('/shop', { 'category': category.slug })} className='border-2 border-black rounded-xl m-3 flex content-center'>
+        <Link href={route('shop.index', {category: category.slug})} className='border-2 border-black rounded-xl m-3 flex content-center'>
             <img className='border-2 border-black m-5 w-24 bg-gray-300' src='http://localhost:8000/images/gun_icon.png' alt='Gun'/>
             <div className='m-auto flex'>
                 <h3 className='text-3xl my-auto font-bold'>{category.name}</h3>
                 <img className='w-5 ml-2' src="http://localhost:8000/images/chevron-right.svg" alt="" />
             </div>
-        </button>
+        </Link>
     );
 }
