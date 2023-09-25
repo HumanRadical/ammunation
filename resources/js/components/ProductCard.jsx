5670ecf117437958619601b6641fa2f1d23e637c
Easton Meth <easton.meth@gmail.com>
2023-09-25 14:36:59 -0400

    Display stars on index

--- resources/js/components/ProductCard.jsx
+++ resources/js/components/ProductCard.jsx
@@ -1,4 +1,5 @@
 import { Link } from '@inertiajs/react';
+import StarBar from './StarBar';
 
 export default function ProductCard({ product }) {
     return (
@@ -8,7 +9,8 @@ export default function ProductCard({ product }) {
                 src={product.image ? `http://localhost:8000/storage/${product.image}` : 'http://localhost:8000/images/gun_icon.png'}
                 alt={product.name}/>
             <h3 className='font-bold text-lg mt-3'>{product.name}</h3>
-            <h4 className='text-gray-500'>{product.category.name}</h4>
+            <StarBar stars={product.stars} />
+            <h4 className='text-gray-500 mt-1'>{product.category.name}</h4>
             <h5 className='text-green-500'>${product.price}</h5>
         </Link>
     );
