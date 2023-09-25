c6505b36e0f7fd6eacb884948d4c67fc2eb4118a
Easton Meth <easton.meth@gmail.com>
2023-09-25 17:20:15 -0400

    Reset form when review submitted

--- resources/js/components/ResponsiveStarBar.jsx
+++ resources/js/components/ResponsiveStarBar.jsx
@@ -1,11 +1,11 @@
-export default function ResponsiveStarBar({ stars, setStars, className = '' }) {
+export default function ResponsiveStarBar({ stars, setData, className = '' }) {
     let starIcons = []
 
     for (let i = 0; i < stars; i++) {
-        starIcons.push(<img src='http://localhost:8000/images/full_star.svg' onClick={() => setStars(i + 1)} alt='Full star' />)
+        starIcons.push(<img src='http://localhost:8000/images/full_star.svg' onClick={() => setData('stars', i + 1)} alt='Full star' />)
     }
     for (let i = stars; i < 5; i++) {
-        starIcons.push(<img src='http://localhost:8000/images/empty_star.svg' onClick={() => setStars(i + 1)} alt='Empty star' />)
+        starIcons.push(<img src='http://localhost:8000/images/empty_star.svg' onClick={() => setData('stars', i + 1)} alt='Empty star' />)
     }
 
     return (
--- resources/js/components/ReviewForm.jsx
+++ resources/js/components/ReviewForm.jsx
@@ -3,19 +3,20 @@ import ResponsiveStarBar from './ResponsiveStarBar';
 import { useEffect, useState } from 'react';
 
 export default function ReviewForm({ auth, product }) {
-    const [stars, setStars] = useState(0)
-
-    const { data, setData, post, errors, processing } = useForm({
-        stars: stars
+    const { data, setData, post, reset, errors, processing } = useForm({
+        body: '',
+        stars: 0
     })
-
-    useEffect(() => {
-        setData('stars', stars)
-    }, [stars])
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        post(`/shop/${product.slug}/reviews`, { preserveState: true })
+        post(`/shop/${product.slug}/reviews`, { 
+            preserveState: true,
+            preserveScroll: true,
+            onSuccess: () => {
+                reset()
+            }
+        })
     }
 
     return (
@@ -27,16 +28,15 @@ export default function ReviewForm({ auth, product }) {
                     <h3 className='text-2xl font-bold'>Add a review:</h3>
                     <form className='flex flex-wrap mt-3' onSubmit={handleSubmit}>
                         <img className='rounded-full w-10 h-10 my-auto' src='http://localhost:8000/images/user_icon.png' alt='User Icon' />
-                        <ResponsiveStarBar className='h-7 my-auto ml-2' stars={stars} setStars={setStars} />
+                        <ResponsiveStarBar className='h-7 my-auto ml-2' stars={data.stars} setData={setData} />
                         {errors.stars && <p className='text-red-500 mt-1 w-full'>{errors.stars}</p>}
                         <textarea 
                             className='w-full mt-3 rounded-lg border-gray-500' 
-                            name='body'
+                            value={data.body}
                             onChange={e => setData('body', e.target.value)} 
+                            placeholder='Describe your experience...'
                             rows='4' 
-                            required
-                            >{data.body}
-                        </textarea>
+                            required />
                         {errors.body && <p className='text-red-500 mt-1 w-full'>{errors.body}</p>}
                         <button 
                             className='bg-blue-500 hover:bg-blue-600 text-white text-xl font-bold rounded-lg px-4 py-2 mt-3'
