43d52557c4bcc0d9235b248a137dd399be94725a
Easton Meth <easton.meth@gmail.com>
2023-10-03 13:09:48 -0400

    Styling

--- resources/js/components/ProductCard.jsx
+++ resources/js/components/ProductCard.jsx
@@ -8,7 +8,7 @@ export default function ProductCard({ product }) {
                 className='border-2 border-black w-full bg-gray-300' 
                 src={product.image ? `/storage/${product.image}` : '/images/gun_icon.png'}
                 alt={product.name}/>
-            <h3 className='font-bold text-xl mt-3'>{product.name}</h3>
+            <h3 className='font-bold text-xl mt-2'>{product.name}</h3>
             { product.reviews.length ? <StarBar stars={product.stars} starBarKey={`product${product.id}`} /> : '' }
             <Link href={route('shop.index', { category: product.category.slug })}>
                 <h4 className='text-white mt-1 hover:underline'>{product.category.name}</h4>
