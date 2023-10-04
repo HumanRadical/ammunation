b49b6358b0e891f08a66a0e4f114c8bc49bd0146
Easton Meth <easton.meth@gmail.com>
2023-10-04 12:33:18 -0400

    Update route

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -15,7 +15,7 @@ export default function Authenticated({ user, header, children }) {
                     <div className="flex justify-between h-16">
                         <div className="flex">
                             <div className="shrink-0 flex items-center">
-                                <Link href="/">
+                                <Link href={route('home')}>
                                     <ApplicationIcon className="block w-10 fill-current text-gray-800" />
                                 </Link>
                             </div>
