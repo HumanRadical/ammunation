6f784186e392b0a573847e6a2bc4c41cd1eed8a7
Easton Meth <easton.meth@gmail.com>
2023-10-06 11:44:01 -0400

    Styling

--- resources/js/components/CategoryCard.jsx
+++ resources/js/components/CategoryCard.jsx
@@ -2,7 +2,10 @@ import { Link } from '@inertiajs/react';
 
 export default function CategoryCard({ category }) {
     return (
-        <Link href={route('shop.index', {category: category.slug})} className='border-2 border-black bg-gray-400 text-black rounded-xl m-3 flex content-center'>
+        <Link 
+            href={route('shop.index', {category: category.slug})} 
+            className='border-2 border-black bg-gray-400 hover:bg-gray-500 transition text-black rounded-xl m-3 flex content-center'
+        >
             <img className='border-2 border-black m-5 w-24 bg-gray-300' src='/images/gun_icon.png' alt='Gun'/>
             <div className='m-auto flex'>
                 <h3 className='text-3xl my-auto font-bold'>{category.name}</h3>
