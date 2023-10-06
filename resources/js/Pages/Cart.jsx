031c1e595ed5c297a4868873f51d2a8d833da2ae
Easton Meth <easton.meth@gmail.com>
2023-10-06 11:26:27 -0400

    Add totals section and update styling

--- resources/js/Pages/Cart.jsx
+++ resources/js/Pages/Cart.jsx
@@ -2,22 +2,44 @@ import MainLayout from '@/Layouts/MainLayout';
 import CartCard from '@/components/CartCard';
 
 export default function Cart({ auth, products }) {
-    const cartCards = products.map(product => {
-        return <CartCard product={product} key={`cartitem${product.id}`} />
-    })
-
     return (
         <MainLayout auth={auth} head='Cart'>
-            <div className='py-10'>
-                <div className='mx-auto rounded-lg w-1/2 py-8 space-y-6 bg-gray-500'>
-                    <h2 className='text-center text-4xl font-bold'>Cart</h2>
-                    <div className='mx-auto px-12 gap-y-4 flex flex-col'>
-                        {cartCards}
-                        <button 
-                            className='bg-orange-500 hover:bg-orange-600 transition text-2xl mt-3 px-3 py-2 rounded-lg self-end'
-                        >Proceed to checkout &rarr;</button>
+            <div className='py-10 flex justify-center mx-auto'>
+                <section className='rounded-lg border-2 border-black py-8 px-12 space-y-6 bg-gray-400 max-w-4xl'>
+                    {
+                        products
+                        ? <table className='px-4'>
+                            <th className='uppercase grid grid-cols-6 text-left'>
+                                <td>Product</td>
+                                <td></td>
+                                <td>Price</td>
+                                <td>Quantity</td>
+                                <td>Total</td>
+                                <td>Options</td>
+                            </th>
+                            <tbody>
+                                {
+                                    products.map(product => {
+                                        return <CartCard product={product} key={`cartitem${product.id}`} />
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                        : <p className='px-20 text-2xl'>Nothing in the cart.</p>
+                    }
+                    
+                </section>
+                <section className='rounded-lg border-2 border-black p-8 space-y-8 bg-gray-400 h-fit'>
+                    <h2 className='text-center text-4xl font-bold'>Totals</h2>
+                    <div className='space-y-2'>
+                        <h3 className='text-xl flex justify-between'>Subtotal: <span>$9000.00</span></h3>
+                        <h3 className='text-xl border-y-2 py-2 flex justify-between'>Tax: <span>$999.99</span></h3>
+                        <h3 className='font-bold text-2xl flex justify-between'>Total: <span>$9999.99</span></h3>
                     </div>
-                </div>
+                    <button 
+                        className='bg-orange-500 hover:bg-orange-600 transition text-2xl mt-3 px-3 py-2 rounded-lg'
+                    >Proceed to checkout &rarr;</button>
+                </section>
             </div>
         </MainLayout>
     )
--- resources/js/components/CartCard.jsx
+++ resources/js/components/CartCard.jsx
@@ -2,23 +2,33 @@ import { Link } from '@inertiajs/react';
 
 export default function CartCard({ product }) {
     return (
-        <div className='border border-black rounded-xl p-5 bg-gray-400 text-black w-full grid grid-cols-7'>
-            <img 
-                className='border-2 border-black bg-gray-300 h-24' 
-                src={product.image ? `/storage/${product.image}` : '/images/gun_icon.png'}
-                alt={product.name}/>
-            <Link className='my-auto col-span-2' href={route('shop.show', { product: product })}>
-                <h3 className='text-2xl font-bold'>{product.name}</h3>
-            </Link>
-            <Link className='my-auto' href={route('shop.index', { category: product.category.slug })}>
-                <h4 className='text-white text-xl hover:underline'>{product.category.name}</h4>
-            </Link>
-            <h5 className='text-green-400 text-xl my-auto'>${product.price}</h5>
-            <label className='flex gap-2 my-auto'>
-                Quantity:
-                <input className='h-6 w-12' type="number" value={1} />
-            </label>
-            <h5 className='justify-self-end my-auto text-blue-600 hover:underline cursor-pointer'>Remove</h5>
-        </div>
+        <tr className='border-t-2 py-5 text-black grid grid-cols-6'>
+            <td className='flex gap-3'>
+                <img 
+                    className='border-2 border-black bg-gray-300 h-28' 
+                    src={product.image ? `/storage/${product.image}` : '/images/gun_icon.png'}
+                    alt={product.name}/>
+            </td>
+            <td className='my-auto'>
+                <Link href={route('shop.show', { product: product })}>
+                    <h3 className='text-2xl font-bold'>{product.name}</h3>
+                </Link>
+                <Link href={route('shop.index', { category: product.category.slug })}>
+                    <h4 className='text-white text-xl hover:underline'>{product.category.name}</h4>
+                </Link>
+            </td>
+            <td className='my-auto'>
+                <h5 className='text-green-400 text-xl'>${product.price}</h5>
+            </td>
+            <td className='flex gap-2 my-auto'>
+                <input className='h-8 w-12 text-lg' type="number" value={1} />
+            </td>
+            <td className='my-auto'>
+                <h5 className='text-green-400 text-xl'>${product.price}</h5>
+            </td>
+            <td className='my-auto'>
+                <h5 className='text-lg text-blue-600 hover:underline cursor-pointer'>Remove</h5>
+            </td>
+        </tr>
     )
 }
