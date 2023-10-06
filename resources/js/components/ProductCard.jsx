775c53360b8ce577289090eda8018a00bb5e2714
Easton Meth <easton.meth@gmail.com>
2023-10-06 13:57:04 -0400

    Add review count to product card and style

--- resources/js/components/ProductCard.jsx
+++ resources/js/components/ProductCard.jsx
@@ -3,13 +3,20 @@ import StarBar from './StarBar';
 
 export default function ProductCard({ product }) {
     return (
-        <Link href={route('shop.show', { product: product })} className='border-2 border-black rounded-xl p-5 bg-gray-400 hover:bg-gray-500 transition text-black space-y-1'>
+        <Link href={route('shop.show', { product: product })} className='border-2 border-black rounded-xl p-6 bg-gray-400 hover:bg-gray-500 transition text-black'>
             <img 
                 className='border-2 border-black w-full bg-gray-300' 
                 src={product.image ? `/storage/${product.image}` : '/images/gun_icon.png'}
                 alt={product.name}/>
-            <h3 className='font-bold text-xl'>{product.name}</h3>
-            { product.reviews.length ? <StarBar stars={product.stars} starBarKey={`product${product.id}`} /> : '' }
+            <h3 className='font-bold text-xl mt-2'>{product.name}</h3>
+            { 
+                product.reviews.length 
+                ? <div className='flex gap-1'>
+                    <StarBar stars={product.stars} starBarKey={`product${product.id}`} />
+                    <span>({product.reviews.length})</span>
+                </div>
+                : ''
+            }
             <Link href={route('shop.index', { category: product.category.slug })}>
                 <h4 className='text-white hover:underline'>{product.category.name}</h4>
             </Link>
