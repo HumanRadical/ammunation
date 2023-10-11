21d06bcee962a8ad8e2fe945405053371a95812f
Easton Meth <easton.meth@gmail.com>
2023-10-11 12:37:10 -0400

    Update cart migrations

--- database/migrations/2023_10_06_190802_create_cart_product_table.php
+++ database/migrations/2023_10_06_190802_create_cart_product_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('cart_product', function (Blueprint $table) {
             $table->primary(['cart_id', 'product_id']);
-            $table->foreignId('cart_id');
-            $table->foreignId('product_id');
+            $table->foreignId('cart_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->integer('quantity')->default(1);
             $table->timestamps();
         });
