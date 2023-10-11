f11ca4f548519e394c7b1260cf6bfeefde9467d9
Easton Meth <easton.meth@gmail.com>
2023-10-11 11:02:06 -0400

    clean up

--- resources/js/components/CartCard.jsx
+++ resources/js/components/CartCard.jsx
@@ -1,5 +1,5 @@
 import { Link, router } from '@inertiajs/react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function CartCard({ product }) {
     const [quantity, setQuantity] = useState(product.pivot.quantity)
