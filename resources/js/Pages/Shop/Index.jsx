c04588c3e22b54e25f416a208d0db32b83e8d5a2
Easton Meth <easton.meth@gmail.com>
2023-10-11 11:53:25 -0400

    Make pagination bar work

--- resources/js/Pages/Shop/Index.jsx
+++ resources/js/Pages/Shop/Index.jsx
@@ -2,8 +2,12 @@ import MainLayout from '@/Layouts/MainLayout';
 import FilterBar from '@/components/FilterBar';
 import PaginationBar from '@/components/PaginationBar';
 import ProductCard from '@/components/ProductCard';
+import { useState } from 'react';
 
 export default function Index({ auth, cartCount, products, manufacturers, categories, query }) {
+    const [currentPage, setCurrentPage] = useState(1)
+    const perPage = 2
+
     return (
         <MainLayout auth={auth} cartCount={cartCount} head='Shop'>
             <FilterBar manufacturers={manufacturers} categories={categories} query={query} />
@@ -12,12 +16,12 @@ export default function Index({ auth, cartCount, products, manufacturers, catego
                 ? <main className='w-2/3 mx-auto py-10 space-y-10'>
                     <div className='grid grid-cols-4 gap-8'>
                         {
-                            products.map(product => {
+                            products.slice((currentPage - 1) * perPage, currentPage * perPage).map(product => {
                                 return <ProductCard product={product} key={`productcard${product.id}`} />
                             })
                         }
                     </div>
-                    <PaginationBar />
+                    <PaginationBar perPage={perPage} itemCount={products.length} currentPage={currentPage} setCurrentPage={setCurrentPage} />
                 </main>
                 : <main className='w-2/3 bg-gray-400 border-2 border-black rounded-xl text-center mx-auto my-10 py-16'>
                     <p className='text-2xl'>Nothing to see here...</p>
--- resources/js/components/PaginationBar.jsx
+++ resources/js/components/PaginationBar.jsx
@@ -1,25 +1,42 @@
-export default function PaginationBar({ size = 10 }) {
+export default function PaginationBar({ size = 10, pageUrl = '/shop', perPage, itemCount, currentPage, setCurrentPage }) {
+    const pageCount = Math.ceil(itemCount / perPage)
+
+    let firstPage
+    if (currentPage < 3) {
+        firstPage = 1
+    } else if (currentPage >= pageCount - 2) {
+        firstPage = pageCount - 4
+    } else {
+        firstPage = currentPage - 2
+    }
+
+    let lastPage = firstPage + 4 < pageCount ? firstPage + 4 : pageCount
+    if (firstPage + 4 < pageCount) {
+        lastPage = firstPage + 4
+    } else {
+        lastPage = pageCount
+    }
+
+    let buttons = []
+    for (let i = firstPage; i <= lastPage; i++) {
+        buttons.push(
+            <button 
+                className={`w-${size} hover:bg-gray-400 transition ${currentPage === i && 'bg-gray-400'}`} 
+                onClick={() => setCurrentPage(i)} 
+                key={`pagebutton${i}`}
+            >
+                {i}
+            </button>
+        )
+    }
+
     return (
         <div className={`max-w-fit mx-auto flex bg-gray-300 divide-x-2 divide-black text-black text-xl h-${size} rounded-lg`}>
-            <button className={`w-${size} hover:bg-gray-400 transition rounded-l-lg`}>
+            <button className={`w-${size} hover:bg-gray-400 transition rounded-l-lg`} onClick={() => currentPage > 1 && setCurrentPage(prevPage => prevPage - 1)}>
                 <img className='w-6 mx-auto' src="/images/chevron_left.svg" alt="Last page" />
             </button>
-            <button className={`w-${size} hover:bg-gray-400 transition`}>
-                1
-            </button>
-            <button className={`w-${size} hover:bg-gray-400 transition`}>
-                2
-            </button>
-            <button className={`w-${size} hover:bg-gray-400 transition`}>
-                3
-            </button>
-            <button className={`w-${size} hover:bg-gray-400 transition`}>
-                4
-            </button>
-            <button className={`w-${size} hover:bg-gray-400 transition`}>
-                5
-            </button>
-            <button className={`w-${size} hover:bg-gray-400 transition rounded-r-lg`}>
+            {buttons}
+            <button className={`w-${size} hover:bg-gray-400 transition rounded-r-lg`} onClick={() => currentPage < pageCount && setCurrentPage(prevPage => prevPage + 1)}>
                 <img className='w-6 mx-auto' src="/images/chevron_right.svg" alt="Last page" />
             </button>
         </div>
