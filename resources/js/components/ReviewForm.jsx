8f192bdc7826024c4cd49464647919264a62ee15
Easton Meth <easton.meth@gmail.com>
2023-10-12 11:56:21 -0400

    Refactor

--- resources/js/components/ReviewForm.jsx
+++ resources/js/components/ReviewForm.jsx
@@ -23,33 +23,31 @@ export default function ReviewForm({ auth, product }) {
             {
                 auth.user
                 ? 
-                <div className='space-y-2'>
+                <form className='space-y-3' onSubmit={handleSubmit}>
                     <h3 className='text-2xl font-bold'>Add a review:</h3>
-                    <form className='space-y-3' onSubmit={handleSubmit}>
-                        <div className='flex flex-wrap'>
-                            <img className='rounded-full w-10 h-10 my-auto' src='/images/user_icon.png' alt='User Icon' />
-                            <ResponsiveStarBar className='h-7 my-auto ml-2' stars={data.stars} setData={setData} />
-                            {errors.stars && <p className='text-red-500 w-full mt-1'>{errors.stars}</p>}
-                        </div>
-                        {
-                            product.reviews.find(review => review.user_id === auth.user.id)
-                            && <p className='text-white'>You've already left a review, update your old one?</p>
-                        }
-                        <textarea 
-                            className='w-full rounded-lg border-gray-500' 
-                            value={data.body}
-                            onChange={e => setData('body', e.target.value)} 
-                            placeholder='Describe your experience...'
-                            rows='4' 
-                            required />
-                        {errors.body && <p className='text-red-500 w-full mt-1'>{errors.body}</p>}
-                        <button 
-                            className='bg-blue-500 hover:bg-blue-600 text-white text-xl font-bold rounded-lg px-4 py-2 mt-3'
-                            disabled={processing}
-                            >SUBMIT
-                        </button>
-                    </form>
-                </div>
+                    <div className='flex flex-wrap'>
+                        <img className='rounded-full w-10 h-10 my-auto' src='/images/user_icon.png' alt='User Icon' />
+                        <ResponsiveStarBar className='h-7 my-auto ml-2' stars={data.stars} setData={setData} />
+                        {errors.stars && <p className='text-red-500 w-full mt-1'>{errors.stars}</p>}
+                    </div>
+                    {
+                        product.reviews.find(review => review.user_id === auth.user.id)
+                        && <p className='text-white'>You've already left a review, update your old one?</p>
+                    }
+                    <textarea 
+                        className='w-full rounded-lg border-gray-500' 
+                        value={data.body}
+                        onChange={e => setData('body', e.target.value)} 
+                        placeholder='Describe your experience...'
+                        rows='4' 
+                        required />
+                    {errors.body && <p className='text-red-500 w-full mt-1'>{errors.body}</p>}
+                    <button 
+                        className='bg-blue-500 hover:bg-blue-600 text-white text-xl font-bold rounded-lg px-4 py-2 mt-3'
+                        disabled={processing}
+                        >SUBMIT
+                    </button>
+                </form>
                 : 
                 <h4 className='text-xl font-bold'>
                     <Link className='text-blue-500 hover:underline' href={route('login')}>Log in</Link> or <Link className='text-blue-500 hover:underline' href={route('register')}>Register</Link> to add a review!
